336a1ad2680f2ee0a0ff332096e10215ed33bd6e
Jaz Martin <jasmin.martin@sky.uk>
2018-12-16 16:11:46 +0000

    Render initial board state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+const initialState = {
+    cells: Array(81).fill('-')
+}
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <Board initialState={initialState}/>
         </header>
       </div>
     );
   }
+}
+
+class Board extends Component {
+    render() {
+        const {initialState} = this.props;
+        const rows = [...Array(9).keys()].map(x => {
+            const start = x * 9;
+            const end = (x + 1) * 9;
+            return initialState.cells.slice(start, end)
+        });
+
+        console.log(rows);
+        return (
+            <div className="Board">
+             <table>
+                 <tbody>
+                    {rows.map(row => {
+                        return(
+                            <SudokuRow cells={row}/>
+                        )
+                    })}
+                 </tbody>
+             </table>
+            </div>
+        );
+    }
+}
+
+class SudokuRow extends Component {
+    render() {
+        const {cells} = this.props;
+        return (
+            <tr>
+                {cells.map(cell => {
+                        return (
+                            <td>
+                                {cell}
+                            </td>
+                        )
+                    })
+                }
+            </tr>
+        )
+    }
 }
 
 export default App;
